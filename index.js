0de692ec55a600edafabeee50cc23efd2bfad2a5
Adrien REDON <redon.adrien@gmail.com>
2016-10-01 21:06:46 +0200

    Simplify promise

--- index.js
+++ index.js
@@ -12,50 +12,47 @@ app.get('/scrape/club/:club_id/week/:week', function(req, res){
         result += '[';
     }).then(function() {
 
-        q.fcall(function() {
-            const options = {
-                uri: 'https://www.fff.fr/la-vie-des-clubs/' + req.params.club_id + '/agenda/semaine-' + req.params.week,
-                headers: {
-                    'User-Agent': 'Request-Promise'
-                },
-            };
+    const options = {
+        uri: 'https://www.fff.fr/la-vie-des-clubs/' + req.params.club_id + '/agenda/semaine-' + req.params.week,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+    };
 
-            request (options, function(error, response, html){
-                let json = {championnat: "", date: "", team1: "", team2: "", score: ""}
-                if(!error){
-                    const $ = cheerio.load(html)
+    request (options, function(error, response, html){
+        let json = {championnat: "", date: "", team1: "", team2: "", score: ""}
+        if(!error){
+            const $ = cheerio.load(html)
 
-                    let championnat, date, team1, team2
+            let championnat, date, team1, team2
 
-                    // We'll use the unique header class as a starting point.
+            // We'll use the unique header class as a starting point.
 
-                    $('h3').filter(function(){
+            $('h3').filter(function(){
 
-                // Let's store the data we filter into a variable so we can easily see what's going on.
+        // Let's store the data we filter into a variable so we can easily see what's going on.
 
-                        const data = $(this)
+                const data = $(this)
 
-                        championnat = data.text();
+                championnat = data.text();
 
-                        if (championnat == 'EXCELLENCE SENIORS - POULE A') {
-                            json.championnat = championnat;
-                            json.date = data.next().find('h4').text();
-                            json.team1 = data.next().find('.eqleft > a').text();
-                            json.team2 = data.next().find('.eqright > a').text();
-                            json.score = data.next().find('.score > .message').text();
+                if (championnat == 'EXCELLENCE SENIORS - POULE A') {
+                    json.championnat = championnat;
+                    json.date = data.next().find('h4').text();
+                    json.team1 = data.next().find('.eqleft > a').text();
+                    json.team2 = data.next().find('.eqright > a').text();
+                    json.score = data.next().find('.score > .message').text();
 
-                            result += JSON.stringify(json, null, 4) + ',';
-                        }
-                    })
+                    result += JSON.stringify(json, null, 4) + ',';
                 }
-            }).then(function() {
-                result += ']';
-                res.send(result);
             })
-
-        })
-
+        }
+    }).then(function() {
+        result += ']';
+        res.send(result);
     })
+
+})
 
 })
 
